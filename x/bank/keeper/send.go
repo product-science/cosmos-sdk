566b6e8cfcc7138a4ed7d094bf028140e4af5036
johnlong <john.long@productscience.ai>
2025-05-08 16:15:23 -0700

    Add better logging for when we have an issue with insufficient funds The way it currently works the error gets logged from a generic location and we have no idea who or what called subUnblockedCoins.

--- x/bank/keeper/send.go
+++ x/bank/keeper/send.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 	"fmt"
+	"runtime"
 
 	"cosmossdk.io/collections"
 	"cosmossdk.io/core/store"
@@ -275,6 +276,15 @@ func (k BaseSendKeeper) subUnlockedCoins(ctx context.Context, addr sdk.AccAddres
 			if len(spendable) == 0 {
 				spendable = sdk.Coins{sdk.NewCoin(coin.Denom, math.ZeroInt())}
 			}
+
+			stack := make([]byte, 2048)
+			runtime.Stack(stack, false)
+			k.logger.Error("insufficient spendable funds",
+				"spendable", spendable,
+				"required", coin,
+				"account", addr.String(),
+				"stack", string(stack))
+
 			return errorsmod.Wrapf(
 				sdkerrors.ErrInsufficientFunds,
 				"spendable balance %s is smaller than %s",
